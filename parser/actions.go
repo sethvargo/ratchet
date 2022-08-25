34167523977282e0148ef373a4b6e8d28bd54658
Seth Vargo <seth@sethvargo.com>
2022-08-25 11:36:06 -0400

    Add support for GitHub Actions composite actions (#38)

--- parser/actions.go
+++ parser/actions.go
@@ -28,61 +28,30 @@ func (a *Actions) Parse(m *yaml.Node) (*RefsList, error) {
 			continue
 		}
 
-		// jobs: keyword
-		for i, jobsMap := range docMap.Content {
-			if jobsMap.Value != "jobs" {
-				continue
-			}
-
-			// Individual job names
-			jobs := docMap.Content[i+1]
-			if jobs.Kind != yaml.MappingNode {
-				continue
-			}
-
-			for _, jobMap := range jobs.Content {
-				if jobMap.Kind != yaml.MappingNode {
+		for i, topLevelMap := range docMap.Content {
+			// runs: keyword
+			if topLevelMap.Value == "runs" {
+				runs := docMap.Content[i+1]
+				if runs.Kind != yaml.MappingNode {
 					continue
 				}
 
-				for j, sub := range jobMap.Content {
-					// Container reference for running the job, should be resolved as a
-					// Docker reference.
-					if sub.Value == "container" {
-						containerMap := jobMap.Content[j+1]
-						for k, property := range containerMap.Content {
-							if property.Value == "image" {
-								image := containerMap.Content[k+1]
-								ref := resolver.NormalizeContainerRef(image.Value)
-								refs.Add(ref, image)
-								break
-							}
-						}
+				// Only look at composite actions.
+				foundComposite := false
+				for j, runMap := range runs.Content {
+					if runMap.Value == "using" && len(runs.Content) > j+1 && runs.Content[j+1].Value == "composite" {
+						foundComposite = true
+						break
 					}
+				}
+				if !foundComposite {
+					continue
+				}
 
-					// CI service container, should be resolved as a Docker reference.
-					// This is a map, so the container value is nested a bit deeper.
-					if sub.Value == "services" {
-						servicesMap := jobMap.Content[j+1]
-						for _, subMap := range servicesMap.Content {
-							if subMap.Kind != yaml.MappingNode {
-								continue
-							}
-
-							for k, property := range subMap.Content {
-								if property.Value == "image" {
-									image := subMap.Content[k+1]
-									ref := resolver.NormalizeContainerRef(image.Value)
-									refs.Add(ref, image)
-									break
-								}
-							}
-						}
-					}
-
-					// List of steps, iterate over each step and find the "uses" clause.
-					if sub.Value == "steps" {
-						steps := jobMap.Content[j+1]
+				// List of steps, iterate over each step and find the "uses" clause.
+				for j, runMap := range runs.Content {
+					if runMap.Value == "steps" {
+						steps := runs.Content[j+1]
 						for _, step := range steps.Content {
 							if step.Kind != yaml.MappingNode {
 								continue
@@ -91,7 +60,7 @@ func (a *Actions) Parse(m *yaml.Node) (*RefsList, error) {
 							for k, property := range step.Content {
 								if property.Value == "uses" {
 									uses := step.Content[k+1]
-									// Only include references to remove workflows. This could be
+									// Only include references to remote workflows. This could be
 									// a local workflow, which should not be pinned.
 									switch {
 									case strings.HasPrefix(uses.Value, "docker://"):
@@ -105,20 +74,96 @@ func (a *Actions) Parse(m *yaml.Node) (*RefsList, error) {
 							}
 						}
 					}
+				}
+			}
 
-					// Top-level uses, likely for a reusable workflow.
-					if sub.Value == "uses" {
-						uses := jobMap.Content[j+1]
+			// jobs: keyword
+			if topLevelMap.Value == "jobs" {
+				jobs := docMap.Content[i+1]
+				if jobs.Kind != yaml.MappingNode {
+					continue
+				}
 
-						// Only include references to remove workflows. This could be a
-						// local workflow, which should not be pinned.
-						switch {
-						case strings.HasPrefix(uses.Value, "docker://"):
-							ref := resolver.NormalizeContainerRef(uses.Value)
-							refs.Add(ref, uses)
-						case strings.Contains(uses.Value, "@"):
-							ref := resolver.NormalizeActionsRef(uses.Value)
-							refs.Add(ref, uses)
+				for _, jobMap := range jobs.Content {
+					if jobMap.Kind != yaml.MappingNode {
+						continue
+					}
+
+					for j, sub := range jobMap.Content {
+						// Container reference for running the job, should be resolved as a
+						// Docker reference.
+						if sub.Value == "container" {
+							containerMap := jobMap.Content[j+1]
+							for k, property := range containerMap.Content {
+								if property.Value == "image" {
+									image := containerMap.Content[k+1]
+									ref := resolver.NormalizeContainerRef(image.Value)
+									refs.Add(ref, image)
+									break
+								}
+							}
+						}
+
+						// CI service container, should be resolved as a Docker reference.
+						// This is a map, so the container value is nested a bit deeper.
+						if sub.Value == "services" {
+							servicesMap := jobMap.Content[j+1]
+							for _, subMap := range servicesMap.Content {
+								if subMap.Kind != yaml.MappingNode {
+									continue
+								}
+
+								for k, property := range subMap.Content {
+									if property.Value == "image" {
+										image := subMap.Content[k+1]
+										ref := resolver.NormalizeContainerRef(image.Value)
+										refs.Add(ref, image)
+										break
+									}
+								}
+							}
+						}
+
+						// List of steps, iterate over each step and find the "uses" clause.
+						if sub.Value == "steps" {
+							steps := jobMap.Content[j+1]
+							for _, step := range steps.Content {
+								if step.Kind != yaml.MappingNode {
+									continue
+								}
+
+								for k, property := range step.Content {
+									if property.Value == "uses" {
+										uses := step.Content[k+1]
+										// Only include references to remote workflows. This could be
+										// a local workflow, which should not be pinned.
+										switch {
+										case strings.HasPrefix(uses.Value, "docker://"):
+											ref := resolver.NormalizeContainerRef(uses.Value)
+											refs.Add(ref, uses)
+										case strings.Contains(uses.Value, "@"):
+											ref := resolver.NormalizeActionsRef(uses.Value)
+											refs.Add(ref, uses)
+										}
+									}
+								}
+							}
+						}
+
+						// Top-level uses, likely for a reusable workflow.
+						if sub.Value == "uses" {
+							uses := jobMap.Content[j+1]
+
+							// Only include references to remote workflows. This could be a
+							// local workflow, which should not be pinned.
+							switch {
+							case strings.HasPrefix(uses.Value, "docker://"):
+								ref := resolver.NormalizeContainerRef(uses.Value)
+								refs.Add(ref, uses)
+							case strings.Contains(uses.Value, "@"):
+								ref := resolver.NormalizeActionsRef(uses.Value)
+								refs.Add(ref, uses)
+							}
 						}
 					}
 				}
--- parser/actions_test.go
+++ parser/actions_test.go
@@ -71,6 +71,24 @@ jobs:
 				"container://ubuntu:20.04",
 			},
 		},
+		{
+			name: "composite",
+			in: `
+runs:
+  using: 'composite'
+  steps:
+    - uses: 'actions/checkout@v3'
+    - uses: 'docker://ubuntu:20.04'
+    - uses: 'docker://ubuntu@sha256:47f14534bda344d9fe6ffd6effb95eefe579f4be0d508b7445cf77f61a0e5724'
+      with:
+        uses: 'foo/bar@v0'
+`,
+			exp: []string{
+				"actions://actions/checkout@v3",
+				"container://ubuntu:20.04",
+				"container://ubuntu@sha256:47f14534bda344d9fe6ffd6effb95eefe579f4be0d508b7445cf77f61a0e5724",
+			},
+		},
 	}
 
 	for _, tc := range cases {

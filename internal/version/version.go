2e8d77cab160aeb59008c5107d11d83e1a26f355
Seth Vargo <seth@sethvargo.com>
2023-11-17 16:36:56 -0500

    Ensure version information is propagated (#61)
    Fixes https://github.com/sethvargo/ratchet/issues/58

--- internal/version/version.go
+++ internal/version/version.go
@@ -7,10 +7,14 @@ import (
 
 var (
 	// Name is the name of the binary.
-	Name = "ratchet"
+	name string
+	Name = valueOrFallback(name, func() string {
+		return "ratchet"
+	})
 
 	// Version is the main package version.
-	Version = func() string {
+	version string
+	Version = valueOrFallback(version, func() string {
 		if info, ok := debug.ReadBuildInfo(); ok {
 			if v := info.Main.Version; v != "" {
 				return v // e.g. "v0.0.1-alpha6.0.20230815191505-8628f8201363"
@@ -18,10 +22,11 @@ var (
 		}
 
 		return "source"
-	}()
+	})
 
 	// Commit is the git sha.
-	Commit = func() string {
+	commit string
+	Commit = valueOrFallback(commit, func() string {
 		if info, ok := debug.ReadBuildInfo(); ok {
 			for _, setting := range info.Settings {
 				if setting.Key == "vcs.revision" {
@@ -31,13 +36,18 @@ var (
 		}
 
 		return "HEAD"
-	}()
+	})
 
 	// OSArch returns the denormalized operating system and architecture.
-	OSArch = func() string {
-		return runtime.GOOS + "/" + runtime.GOARCH
-	}()
+	OSArch = runtime.GOOS + "/" + runtime.GOARCH
 
 	// HumanVersion is the compiled version.
 	HumanVersion = Name + " " + Version + " (" + Commit + ", " + OSArch + ")"
 )
+
+func valueOrFallback(val string, fn func() string) string {
+	if val != "" {
+		return val
+	}
+	return fn()
+}

9b0a610e9be65620f59831888c753d6905b8dc54
pdewilde <pdewilde@google.com>
2025-02-11 13:14:12 -0800

    Add to test

--- command/command_test.go
+++ command/command_test.go
@@ -24,6 +24,7 @@ func Test_loadYAMLFiles(t *testing.T) {
 		"drone.yml":               "",
 		"github-crazy-indent.yml": "github.yml",
 		"github-issue-80.yml":     "",
+		"github-subdirectory.yml": "",
 		"github.yml":              "",
 		"gitlabci.yml":            "",
 		"no-trailing-newline.yml": "no-trailing-newline.golden.yml",
